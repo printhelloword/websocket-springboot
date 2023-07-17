cca9dae0dd337c030d5b0951b94ab6995dc60fcb
sles <salessudjono@gmail.com>
2023-07-17 15:33:16 +0700

    config, endpoint  and fe

--- src/main/java/com/phw/websocket/GreetingController.java
+++ src/main/java/com/phw/websocket/GreetingController.java
@@ -2,6 +2,7 @@ package com.phw.websocket;
 
 import com.phw.websocket.dto.Greeting;
 import com.phw.websocket.dto.HelloMessage;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -10,12 +11,14 @@ import org.springframework.web.util.HtmlUtils;
 
 @Controller
 @RequestMapping("api")
+@Log4j2
 public class GreetingController {
 
     @MessageMapping("hello")
     @SendTo("/topic/greetings")
     public Greeting greeting (HelloMessage helloMessage) throws Exception {
         Thread.sleep(1000);
+        log.info("New Message : "+helloMessage.getName());
         return new Greeting("Hello, " + HtmlUtils.htmlEscape(helloMessage.getName() + "!"));
     }
 
